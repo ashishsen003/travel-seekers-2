dce23efef11f0dad5831b04d6a4247b927e3e069
ashishsen003 <ashishsen997@gmail.com>
2024-04-26 17:39:05 +0530

    login page fixed

--- my-app/src/router/Routers.jsx
+++ my-app/src/router/Routers.jsx
@@ -3,7 +3,7 @@ import {Navigate, Route, Routes} from 'react-router-dom'
 import Home from '../pages/Home'
 import Tours from '../pages/Tours'
 import TourDetails from '../pages/TourDetails';
-import { Login } from '../pages/Login';
+import Login from '../pages/Login';
 import {Register} from "../pages/Register"
 import SearchResultList from '../pages/SearchResultList'
 import { Thankyou } from '../pages/Thankyou'
