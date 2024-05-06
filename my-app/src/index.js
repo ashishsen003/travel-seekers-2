6b8de1208f1c6292923714dfc2363a4ad398d8ea
ashishsen003 <ashishsen997@gmail.com>
2024-05-06 16:06:03 +0530

    auth context setup

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,29 @@
 // import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </AuthContextProvider>
   </ChakraProvider>
 );
 
