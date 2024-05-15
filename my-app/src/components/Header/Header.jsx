c4488a300572093a397adcf92010cbe420ee711e
ashishsen003 <ashishsen997@gmail.com>
2024-05-15 23:20:59 +0530

    responsive navbar added

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -22,6 +22,7 @@ const nav__link = [
 
 const Header = () => {
   const headerRef = useRef(null);
+  const menuRef = useRef(null);
   const navigate = useNavigate();
   const { user, dispatch } = useContext(AuthContext);
 
@@ -48,6 +49,8 @@ const Header = () => {
     return window.removeEventListener("scroll", stickyHeaderFunc);
   });
 
+  const toggleMenu = ()=>menuRef.current.classList.toggle('show__menu')
+
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -59,7 +62,7 @@ const Header = () => {
             </div>
             {/* =======logo end======= */}
             {/* =======menu start======= */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-5">
                 {nav__link.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -97,7 +100,7 @@ const Header = () => {
                   </>
                 )}
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i className="ri-menu-line"></i>
               </span>
             </div>
